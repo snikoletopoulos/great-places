a027bd0cc08800bc38696c1a7b023ec78a6e1781
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 20:03:54 +0300

    Configure navigation screens

--- App.tsx
+++ App.tsx
@@ -5,17 +5,53 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import AllPlaces from "screens/AllPlaces";
 import AddPlace from "screens/AddPlace";
+import IconButton from "components/ui/IconButton";
+import { Colors } from "constants/colors";
 
-const Stack = createNativeStackNavigator();
+export type PlacesParamList = {
+	AllPlaces: undefined;
+	AddPlace: undefined;
+};
 
-const App = () => {
+const Stack = createNativeStackNavigator<PlacesParamList>();
+
+const App: React.FC = () => {
 	return (
 		<>
 			<StatusBar style="dark" />
 			<NavigationContainer>
-				<Stack.Navigator initialRouteName="AllPlaces">
-					<Stack.Screen name="AllPlaces" component={AllPlaces} />
-					<Stack.Screen name="AddPlace" component={AddPlace} />
+				<Stack.Navigator
+					initialRouteName="AllPlaces"
+					screenOptions={{
+						headerStyle: {
+							backgroundColor: Colors.primary500,
+						},
+						headerTintColor: Colors.gray700,
+						contentStyle: { backgroundColor: Colors.gray700 },
+					}}
+				>
+					<Stack.Screen
+						name="AllPlaces"
+						component={AllPlaces}
+						options={({ navigation }) => ({
+							title: "Your Favorite Places",
+							headerRight: ({ tintColor }) => (
+								<IconButton
+									icon="add"
+									size={24}
+									color={tintColor ?? ""}
+									onPress={() => navigation.navigate("AddPlace")}
+								/>
+							),
+						})}
+					/>
+					<Stack.Screen
+						name="AddPlace"
+						component={AddPlace}
+						options={{
+							title: "Add a new place",
+						}}
+					/>
 				</Stack.Navigator>
 			</NavigationContainer>
 		</>
