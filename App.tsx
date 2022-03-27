8eb3da92646e114666ea4d53bc7f9a3d2e6b9a59
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 15:42:17 +0300

    Format code

--- App.tsx
+++ App.tsx
@@ -1,22 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<Text>Open up App.tsx to start working on your app!</Text>
+		</View>
+	);
+};
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
