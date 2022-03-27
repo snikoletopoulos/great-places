17e9dfc8fc9be2ec29a16e5e9fdb627a3b50b482
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 15:07:33 +0300

    Add base babel and tsconfig files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+// @ts-ignore
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"module-resolver",
+				{
+					alias: {
+						components: "./app/components",
+						assets: "./app/assets",
+						navigation: "./app/navigation",
+						screens: "./app/screens",
+						data: "./app/data",
+						models: "./app/models",
+						constants: "./app/constants",
+						store: "./app/store",
+						types: "./app/types",
+						helpers: "./app/helpers",
+					},
+				},
+			],
+			["react-native-reanimated/plugin"],
+		],
+	};
 };
