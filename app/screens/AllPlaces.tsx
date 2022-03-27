255fece8028212a97d089a1ae98aa83bc588e89d
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 20:02:28 +0300

    Add navigation types

--- app/screens/AllPlaces.tsx
+++ app/screens/AllPlaces.tsx
@@ -1,6 +1,11 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { PlacesParamList } from "App";
+
 import PlacesList from "components/places/PlacesList";
 
-const AllPlaces: React.FC = () => {
+type Props = NativeStackScreenProps<PlacesParamList, "AllPlaces">;
+
+const AllPlaces: React.FC<Props> = () => {
 	return <PlacesList places={[]} />;
 };
 
