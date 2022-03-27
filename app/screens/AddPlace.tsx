882fbe2d1edd1fbc0d0b5ebfcb371cc0e47c1932
Stavros <snikoletopoulos@hotmail.com>
2022-03-27 20:02:14 +0300

    Render PlaceForm component

--- app/screens/AddPlace.tsx
+++ app/screens/AddPlace.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { PlacesParamList } from "App";
 
-const AddPlace: React.FC = () => {
-	return (
-		<View>
-			<Text>Add Place</Text>
-		</View>
-	);
+import PlaceForm from "components/places/PlaceForm";
+
+type Props = NativeStackScreenProps<PlacesParamList, "AddPlace">;
+
+const AddPlace: React.FC<Props> = () => {
+	return <PlaceForm />;
 };
 
 export default AddPlace;
